6e1ff8e5c8f3d5a48648b256cb4a5a3252d74e84
monisha1202 <monishamittal1202@gmail.com>
2025-04-10 23:59:44 -0500

    Violin Plot script

---
+++ ViolinPlot.R
@@ -0,0 +1,13 @@
+#Load packages
+library(Seurat)
+library(tidyverse)
+library(ggplot2)
+library(RColorBrewer)
+library(ggpubr)
+
+
+seurat_obj <- readRDS("seurat_obj.RDS")
+gene_list <- c("gene1","gene2","gene3")
+
+#DotPlot
+VlnPlot(seurat_obj, features = gene_list) 
